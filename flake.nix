36a6f1d014efd6c5f78485123a005da49d398cbd
Naim <naimssj4@gmail.com>
2025-05-09 15:47:10 +0200

    lua begin

--- flake.nix
+++ flake.nix
@@ -4,8 +4,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-    ags = {
-      url = "github:aylur/ags";
+    astal = {
+      url = "github:aylur/astal";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -14,59 +14,101 @@
     {
       self,
       nixpkgs,
-      ags,
+      astal,
     }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
-      packages.${system} = {
-        default = ags.lib.bundle {
-          inherit pkgs;
-          src = ./.;
-          name = "my-shell";
-          entry = "app.ts";
+      packages.${system}.default = astal.lib.mkLuaPackage {
+        inherit pkgs;
+        name = "luabar"; # how to name the executable
+        src = ./lua/oui; # should contain init.lua
 
-          # additional libraries and executables to add to gjs' runtime
-          extraPackages = [
-            ags.packages.${system}.battery
-            ags.packages.${system}.hyprland
-            ags.packages.${system}.mpris
-            ags.packages.${system}.wireplumber
-            ags.packages.${system}.network
-            ags.packages.${system}.tray
-            ags.packages.${system}.bluetooth
-            ags.packages.${system}.notifd
-            pkgs.nerd-fonts.ubuntu-mono
-            pkgs.nerd-fonts.ubuntu
-            # pkgs.fzf
-          ];
-        };
+        extraPackages = with astal.packages.${system}; [
+          battery
+          astal3
+          io
+          apps
+          bluetooth
+          mpris
+          network
+          notifd
+          powerprofiles
+          tray
+          wireplumber
+          hyprland
+        ];
       };
-
       devShells.${system} = {
         default = pkgs.mkShell {
           buildInputs = [
-            # includes all Astal libraries
-            ags.packages.${system}.agsFull
-            ags.packages.${system}.battery
-            ags.packages.${system}.hyprland
-            ags.packages.${system}.mpris
-            ags.packages.${system}.wireplumber
-            ags.packages.${system}.network
-            ags.packages.${system}.tray
-            ags.packages.${system}.bluetooth
-            ags.packages.${system}.notifd
-
-            # includes astal3 astal4 astal-io by default
-            # (ags.packages.${system}.default.overrideAttrs {
-            #   extraPackages = [
-            # cherry pick packages
-            # ];
-            # })
+            astal.packages.${system}.default
           ];
         };
       };
     };
 }
+
+#   outputs =
+#     {
+#       nixpkgs,
+#       ags,
+#       astal,
+#     }:
+#     let
+#       system = "x86_64-linux";
+#       pkgs = nixpkgs.legacyPackages.${system};
+#     in
+#     {
+#       packages.${system} = {
+#         default = ags.lib.bundle {
+#           inherit pkgs;
+#           src = ./.;
+#           name = "my-shell";
+#           entry = "app.ts";
+#
+#           # additional libraries and executables to add to gjs' runtime
+#           extraPackages = [
+#             ags.packages.${system}.battery
+#             ags.packages.${system}.hyprland
+#             ags.packages.${system}.mpris
+#             ags.packages.${system}.wireplumber
+#             ags.packages.${system}.network
+#             ags.packages.${system}.tray
+#             ags.packages.${system}.bluetooth
+#             ags.packages.${system}.notifd
+#             pkgs.nerd-fonts.ubuntu-mono
+#             pkgs.nerd-fonts.ubuntu
+#             # pkgs.fzf
+#           ];
+#         };
+#       };
+#
+#       devShells.${system} = {
+#         default = pkgs.mkShell {
+#           buildInputs = [
+#             astal.packages.${system}.default
+#             # includes all Astal libraries
+#             ags.packages.${system}.agsFull
+#             ags.packages.${system}.battery
+#             ags.packages.${system}.hyprland
+#             ags.packages.${system}.mpris
+#             ags.packages.${system}.wireplumber
+#             ags.packages.${system}.network
+#             ags.packages.${system}.tray
+#             ags.packages.${system}.bluetooth
+#             ags.packages.${system}.notifd
+#
+#             # includes astal3 astal4 astal-io by default
+#             # (ags.packages.${system}.default.overrideAttrs {
+#             #   extraPackages = [
+#             # cherry pick packages
+#             # ];
+#             # })
+#           ];
+#         };
+#       };
+#     };
+# }
